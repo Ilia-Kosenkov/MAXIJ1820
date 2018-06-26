8d1275daa40e412ddb99a227c4da04ae79b1447e
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-26 10:07:31 +0200

    New data input

--- MAXI_J1820/Source/Initialize.R
+++ MAXI_J1820/Source/Initialize.R
@@ -21,22 +21,43 @@
     if (!dir.exists(file.path("Data")))
         stop("No `Data` directory found.")
 
-    fieldStars <<- read.table(file.path("Data", "field_stars.dat"),
+    idDig <- 2
+
+    IdConv <- function(x)
+        x %>%
+            divide_by(10 ^ idDig) %>%
+            map_dbl(~.x - floor(.x)) %>%
+            multiply_by(10 ^ idDig) %>%
+            round %>%
+            as.integer
+
+    starCoords <<- read.table(file.path("Data", "star_stats.dat"),
+                        TRUE, stringsAsFactors = FALSE) %>%
+                as.tibble %>%
+                mutate(ID = IdConv(NO))
+
+    starPol <<- read.table(file.path("Data", "star_pol.dat"),
         TRUE, stringsAsFactors = FALSE) %>%
-        as.tibble
+        as.tibble %>%
+        mutate(ID = if_else(NO < 700, 600L, NO)) %>%
+        mutate(ID = IdConv(ID))
+
+    Bands <<- read_table(file.path("Data", "Bands.dat"),
+                        col_types = cols())
 }
 
 .PrepareData <- function(
     xrange = c(1, 600),
     yrange = c(1, 600),
     degSize = c(5, 5) / 60.0) {
-    fieldStars <<- fieldStars %>%
+    starCoords <<- starCoords %>%
         mutate(RA_D = Ra2Degrees(RA)) %>%
         mutate(DEC_D = Dec2Degrees(DEC)) %>%
         mutate(PX_D = - 0.5 * degSize[1] +
             (PX - xrange[1]) / diff(xrange) * degSize[1]) %>%
         mutate(PY_D = - 0.5 * degSize[2] +
-            (PY - yrange[1]) / diff(yrange) * degSize[2]) %>%
+            (PY - yrange[1]) / diff(yrange) * degSize[2])
+    starPol <<- starPol %>%
         mutate(XPol = p * cos(2 * pa / 180 * pi)) %>%
         mutate(YPol = p * sin(2 * pa / 180 * pi))
 
@@ -52,5 +73,5 @@ if (!exists(".IsInitialized") ||
     .LoadData()
     .PrepareData()
 
-    .IsInitialized <<- TRUE
+    .IsInitialized <<- FALSE
 }
