be326df34f650061cb78f9607be2c34aa6279d4c
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-07-10 18:07:09 +0300

    Preparing averaging data

--- MAXI_J1820/Source/Averages.R
+++ MAXI_J1820/Source/Averages.R
@@ -1,48 +1,133 @@
-GetRawFileNames <- function(path) {
+GetRawFileNames <- function(path, pattern = ".*maxi?18([bvr])([0-9]+).csv") {
     fls <- dir(path, "*.csv", full.names = TRUE)
 
-    obsBeforeFls <- fls %>%
-        str_detect("maxi[0-9]{2}[bvr]{1}") %>% {
-            extract(fls, .)
-        }
+    fls %>% str_match_all(pattern) %>%
+        discard(is_empty) %>%
+        map(as.tibble) %>%
+        bind_rows %>%
+        setNames(c("Path", "Band", "ID")) %>%
+        mutate(Band = toupper(Band), ID = as.integer(ID))
+}
 
-    obsAfterFls <- fls %>%
-        str_detect("max18[bvr]{1}[0-9]{2}") %>% {
-            extract(fls, .)
-        }
+GetFileSizes <- function(path) {
+    data <- path %>% map_int(~read_csv(.x, col_types = cols()) %>%
+        nrow())
+}
 
-    obsBefore <- obsBeforeFls %>%
-        str_match(".*maxi[0-9]{2}([bvr]{1}).*") %>% {
-            map(c("b", "v", "r"), function(x) .[.[, 2] == x, ])
-        } %>% 
-        map(~extract(.x, , 1)) %>%
-        map(function(x) map(x, read_csv, col_types = cols()) %>% bind_rows) %>%
-        setNames(nm = c("b", "v", "r"))
+GenerateInputFiles <- function(files, bandInfo = Bands, idPrefix = 600) {
+    writeFile <- function(data, path) {
+        sink(path)
+        seq.int(length.out = nrow(data)) %>%
+            map(~sprintf("%s\n1%4d%4d%2d\n",
+                data %>% extract(.x, "Path"),
+                data %>% extract(.x, "FlSz") %>% as.integer,
+                data %>% extract(.x, "ID") %>% as.integer + idPrefix,
+                data %>% extract(.x, "BandID") %>% as.integer)) %>%
+            map(cat)
+        cat("end")
+        sink()
+    }
 
-    obsAfter <- obsAfterFls %>%
-        str_match(".*max18([bvr]{1}).*") %>% {
-            map(c("b", "v", "r"), function(x) .[.[, 2] == x,])
-        } %>%
-        map(~extract(.x,, 1)) %>%
-        map(function(x) map(x, read_csv, col_types = cols()) %>% bind_rows) %>%
-        setNames(nm = c("b", "v", "r"))
 
-    obsBefore %>%
-        walk2(names(.),
-              function(data, name)
-                  write_csv(data,
-                            file.path(path,
-                                      paste0("comb_before_", name, ".csv"))))
+    bands <- files %>%
+        pull(Band) %>%
+        unique
 
-    obsAfter %>%
-        walk2(names(.),
-              function(data, name)
-                  write_csv(data,
-                            file.path(path,
-                                      paste0("comb_after_", name, ".csv"))))
+    bands %>%
+        map(~filter(files, Band == .x) %>% arrange(ID)) %>%
+        setNames(bands) %>%
+        map(~writeFile(.x,
+            sprintf("%sin.txt", .x %>% extract(1, "Band"))))
+}
 
+GatherRawOutput <- function(path) {
+    files <- dir(path,
+        pattern = "(in\\.txt)|(lpo.\\.txt)|(te.\\.txt)|(res_.\\.txt)",
+        full.names = TRUE,
+        ignore.case = TRUE)
+    dirPath <- file.path("Output", "RAW")
+
+    if (!dir.exists(dirPath))
+        dir.create(dirPath)
+
+    files %>%
+        map(~sprintf("unix2dos %s", .x)) %>%
+        walk(ExecuteUnix)
+
+    files %>%
+        walk(~file.copy(.x, dirPath, overwrite = TRUE))
+
+    files %>%
+        walk(file.remove)
+}
+
+ApplyCorrections <- function(bandInfo = Bands,
+                starFile = file.path("Test", "maxi.sta")) {
+    bandInfo %>%
+        pull(Band) %>%
+        map(~sprintf("printf \'%s\\nte%s.txt\\n0\\n0\\n\' | %s && %s",
+                starFile,
+                .x,
+                file.path(".", "Binary", "koko.out"),
+                sprintf("mv PRKOKO.txt res_%s.txt", .x))) %>%
+        walk(ExecuteUnix)
+}
+
+ProcessFiles <- function(files) {
+    GenerateInputFiles(files)
+    Bands %>%
+        pull(Band) %>%
+        map(~file.path(".", "Binary", sprintf("polco%s.out", .x))) %>%
+        walk(ExecuteUnix)
+
+    ApplyCorrections()
+
+    GatherRawOutput(".")
+}
+
+SplitInTwo <- function(files, date, bandInfo = Bands) {
+
+    bandInfo %>%
+        pull(Band) %>%
+        map(~filter(files, Band == .x) %>% arrange(ID)) %>%
+        setNames(bandInfo %>% pull(Band)) %>%
+        map(~pull(.x, Path)) %>%
+        map(function(x) map(x, read_csv, col_types = cols())) %>%
+        map(bind_rows) %>%
+        map(function(x)
+            list(Before = filter(x, `T (JD)` <= date),
+                 After = filter(x, `T (JD)` > date)))
+}
+
+PrepareAvgData <- function(date = 2458222.5) {
+    rawFiles <- GetRawFileNames(file.path("Test", "RAW")) %>%
+        mutate(FlSz = GetFileSizes(Path)) %>%
+        left_join(Bands, by = "Band", suffix = c("", ".bnd")) %>%
+        rename(BandID = ID.bnd)
+
+    dirName <- file.path("Data", "RunTime")
+
+    if (!dir.exists(dirName))
+        dir.create(dirName, recursive = TRUE)
+
+    splits <- rawFiles %>% SplitInTwo(date)
+
+    splits %>%
+        walk2(Bands %>% pull(Band),
+            function(x, b) {
+                x %>%
+                extract2("Before") %>%
+                write_csv(path =
+                    file.path(dirName, sprintf("maxi_before_%s.txt", b)))
+                x %>%
+                extract2("After") %>%
+                write_csv(path =
+                    file.path(dirName, sprintf("maxi_after_%s.txt", b)))
+            })
 }
 
 if (IsRun()) {
-    GetRawFileNames(file.path("Test", "RAW"))
+  
+    PrepareAvgData()
+   #ProcessFiles(rawFiles)
 }
